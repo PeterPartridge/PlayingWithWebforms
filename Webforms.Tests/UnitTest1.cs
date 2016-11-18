905abb2865b39774219f58459b1d7abc504afbfd
Peter Partridge <petertoby@aol.com>
2016-11-18 19:06:32 +0000

    Trying to test web forms

--- Webforms.Tests/UnitTest1.cs
+++ Webforms.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Webforms.Models;
+using Webforms.BusinessLogic;
 
 namespace Webforms.Tests
 {
@@ -9,7 +11,24 @@ namespace Webforms.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            productsLogic prolog = new productsLogic();
 
+            prolog.listProducts(500000);
+            prolog.listProducts(-200000);
+            prolog.listProducts(null);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            productsLogic prolog = new productsLogic();
+
+            prolog.productGetItem(2000000);
+            prolog.productGetItem(-200000);
+            prolog.productGetItem(null);
+        }
+
+
+       
     }
 }
